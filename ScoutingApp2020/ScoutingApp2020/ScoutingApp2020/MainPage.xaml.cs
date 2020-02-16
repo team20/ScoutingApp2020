c86a1aac47a4b26d72008dbf9dae6114127e223f
Andrew S <yipten@outlook.com>
2020-02-16 18:21:18 -0500

    submit & reset methods

--- ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/MainPage.xaml.cs
+++ ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Content;
+using System;
 using System.IO;
 using Xamarin.Forms;
 
@@ -33,7 +34,41 @@ namespace ScoutingApp2020 {
 		}
 
 		private void ResetAll() {
-
+			// start
+			ScoutName.Text = "";
+			MatchNumber.Text = "";
+			ReplayMatch.IsToggled = false;
+			TeamNumber.Text = "";
+			AllianceColorPicker.SelectedIndex = -1;
+			StartPosition.Text= "";
+			Preloaded.Text = "";
+			// auto
+			InitLine.IsToggled = false;
+			AutoLower.Text = "0";
+			AutoOuter.Text = "0";
+			AutoInner.Text = "0";
+			AutoMissed.Text = "0";
+			AutoDropped.Text = "0";
+			AutoCollected.Text = "0";
+			// teleop
+			TeleLower.Text = "0";
+			TeleOuter.Text = "0";
+			TeleInner.Text = "0";
+			TeleMissed.Text = "0";
+			TeleDropped.Text = "0";
+			TeleCollected.Text = "0";
+			RotationControl.IsToggled = false;
+			PositionControl.IsToggled = false;
+			// endgame
+			// TODO: fix climb pickers
+			EndHelped.IsToggled = false;
+			EndAssist.IsToggled = false;
+			DefenseAmountPicker.SelectedIndex = -1;
+			DefenseSkillPicker.SelectedIndex = -1;
+			DefendedAmountPicker.SelectedIndex = -1;
+			DefendedSkillPicker.SelectedIndex = -1;
+			BreakdownPicker.SelectedIndex = -1;
+			CommentsEntry.Text = "";
 		}
 
 		#endregion
@@ -93,15 +128,41 @@ namespace ScoutingApp2020 {
 		}
 
 		private async void Button_Clicked(object sender, EventArgs e) {
-			if (ScoutNameEntry.Text == "" ||
+			if (ScoutName.Text == "" ||
 				MatchNumber.Text == "" ||
-				teamNoEntry.Text == "" ||
+				TeamNumber.Text == "" ||
 				AllianceColorPicker.SelectedIndex == -1||
-				startPosEntry.Text == "" || 
-				numPreloadsEntry.Text==""||
-				ParkPicker.SelectedIndex == -1 ||
-				)
+				StartPosition.Text == "" || 
+				Preloaded.Text==""||
+				ClimbZoneAttemptedPicker.SelectedIndex == -1 ||
+				ClimbZoneAchievedPicker.SelectedIndex == -1 ||
+				BreakdownPicker.SelectedIndex == -1 ||
+				NewFilePicker.SelectedIndex == -1)
 				await DisplayAlert("Error", "Not all data entries are filled", "OK");
+			else {
+				_data.ScoutName = ScoutName.Text;
+				_data.MatchNumber = int.Parse(MatchNumber.Text);
+				_data.TeamNumber = int.Parse(TeamNumber.Text);
+				_data.AllianceColor = (string)AllianceColorPicker.SelectedItem;
+				_data.StartPosition = int.Parse(StartPosition.Text);
+				_data.Preloaded = int.Parse(Preloaded.Text);
+				// TODO: fix climb options
+				_data.Breakdown = (string)BreakdownPicker.SelectedItem;
+				_data.Comments = CommentsEntry.Text;
+
+				_data.BuildQuery();
+				_data.BuildString("\t");
+				try {
+					_data.WriteToDatabase();
+					_data.WriteToTextFile(NewFilePicker.SelectedIndex == 0);
+					ResetAll();
+					CurrentPage = new MainPage();
+					await DisplayAlert("Saved", "The data you entered has been saved to a file", "OK");
+				} catch (UnauthorizedAccessException) {
+					if (await DisplayAlert("Error", "App does not have permission to access device storage. To fix this, go to \"Settings > Apps > ScoutingApp2019.Android > Permissions\" and turn on the switch for \"Storage\".", "Settings", "Cancel"))
+						Android.App.Application.Context.StartActivity(new Intent(Android.Provider.Settings.ActionApplicationDetailsSettings, Android.Net.Uri.Parse("package:" + Android.App.Application.Context.PackageName)));
+				}
+			}
 		}
 	}
 }
