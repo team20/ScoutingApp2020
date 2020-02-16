102142317555ea4342c0a81eff4df726e0571f62
Andrew S <yipten@outlook.com>
2020-02-16 15:43:18 -0500

    data handler update
    *data handler writes to txt & sqlite files
    *team number checking added
    *update mono.android version to 8.1 to match tablets
    *update netstandard2.0 to netstandard2.1

--- ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/MainPage.xaml.cs
+++ ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/MainPage.xaml.cs
@@ -2,54 +2,52 @@
 using System.IO;
 using Xamarin.Forms;
 
-namespace ScoutingApp2020
-{
-    public partial class MainPage : TabbedPage
-    {
-        #region Main
-        private readonly DataHandler _data;
+namespace ScoutingApp2020 {
+	public partial class MainPage : TabbedPage {
+		#region Main
+		private readonly DataHandler _data;
 
-        private readonly string[] _teams;
+		private readonly string[] _teams;
 
-        public MainPage()
-        {
-            InitializeComponent();
+		public MainPage() {
+			InitializeComponent();
 
-            _data = new DataHandler("/storage/emulated/0/Download/ScoutingData/", "2020_test");
-            StreamReader streamReader = new StreamReader(Android.App.Application.Context.Assets.Open("2020_detroit_curie_teams.txt"));
-            _teams = streamReader.ReadLine().ToString().Split(',');
-            streamReader.Close();
-            streamReader.Dispose();
-            ResetAll();
-        }
+			_data = new DataHandler("/storage/emulated/0/Download/ScoutingData/", "2020_test");
+			StreamReader streamReader = new StreamReader(Android.App.Application.Context.Assets.Open("Teams.txt"));
+			_teams = streamReader.ReadLine().ToString().Split(',');
+			streamReader.Close();
+			streamReader.Dispose();
+			ResetAll();
+		}
 
-        private void MainTabbedPage_CurrentPageChanged(object sender, EventArgs e)
-        {
-            switch (MainTabbedPage.CurrentPage.TabIndex)
-            {
-                case 0:
-                    BarBackgroundColor = new Color(0.0, 0.6, 0.0);
-                    break;
-                case 1:
-                    BarBackgroundColor = new Color(0.5, 0.3, 0.0);
-                    break;
-                case 2:
-                    BarBackgroundColor = new Color(0.7, 0.0, 0.0);
-                    break;
-                case 3:
-                    BarBackgroundColor = new Color(0.0, 0.0, 0.6);
-                    break;
-                default:
-                    BarBackgroundColor = new Color(0.0, 0.0, 0.0);
-                    break;
-            }
-        }
+		private void MainTabbedPage_CurrentPageChanged(object sender, EventArgs e) {
+			BarBackgroundColor = MainTabbedPage.CurrentPage.TabIndex switch
+			{
+				0 => new Color(0.0, 0.6, 0.0),
+				1 => new Color(0.5, 0.3, 0.0),
+				2 => new Color(0.7, 0.0, 0.0),
+				3 => new Color(0.0, 0.0, 0.6),
+				_ => new Color(0.0, 0.0, 0.0),
+			};
+		}
 
-        private void ResetAll()
-        {
+		private void ResetAll() {
 
-        }
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private void teamNoEntry_Unfocused(object sender, FocusEventArgs e) {
+			bool valid = false;
+			foreach (string team in _teams)
+				if (teamNoEntry.Text == team || teamNoEntry.Text == "") {
+					valid = true;
+					break;
+				}
+			if (!valid) {
+				DisplayAlert("Invalid Team Number", "The team number you entered does not match any of the teams at this event", "OK");
+				teamNoEntry.Focus();
+			}
+		}
+	}
 }
