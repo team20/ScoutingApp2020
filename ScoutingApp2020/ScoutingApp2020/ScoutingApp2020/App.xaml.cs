029b3fb9f982775cc063343c506c9bcfb86d460d
Andrew S <yipten@outlook.com>
2020-02-16 13:53:16 -0500

    update version & some datahandler stuff

--- ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/App.xaml.cs
+++ ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/App.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
-namespace ScoutingApp2020
-{
-    public partial class App : Application
-    {
-        public App()
-        {
+namespace ScoutingApp2020 {
+    public partial class App : Application {
+        public App() {
             InitializeComponent();
 
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
-        {
+        protected override void OnStart() {
         }
 
-        protected override void OnSleep()
-        {
+        protected override void OnSleep() {
         }
 
-        protected override void OnResume()
-        {
+        protected override void OnResume() {
         }
     }
 }
