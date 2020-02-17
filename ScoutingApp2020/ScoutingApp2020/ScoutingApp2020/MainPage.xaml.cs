96d50737faf13e82e047daaab7cccb014c4f2264
Steve9921 <47502601+Steve9921@users.noreply.github.com>
2020-02-16 19:49:53 -0500

    A LOT OF STUFF

--- ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/MainPage.xaml.cs
+++ ScoutingApp2020/ScoutingApp2020/ScoutingApp2020/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using Xamarin.Forms;
 
 namespace ScoutingApp2020 {
 	public partial class MainPage : TabbedPage {
+
 		#region Main
 		private readonly DataHandler _data;
 		private const int MAX = 99;
@@ -36,7 +37,7 @@ namespace ScoutingApp2020 {
 
 		}
 
-		#endregion
+		
 
 		private void teamNoEntry_Unfocused(object sender, FocusEventArgs e) {
 			bool valid = false;
@@ -50,7 +51,9 @@ namespace ScoutingApp2020 {
 				TeamNumber.Focus();
 			}
 		}
+		#endregion
 
+		#region Auto
 		//AUTO INNER MINUS
 		private void AutoInnerMinus_Clicked(object sender, EventArgs e) {
 			if (_data.AutoInner > MIN) {
@@ -67,29 +70,178 @@ namespace ScoutingApp2020 {
 
 		//AUTO OUTER MINUS
 		private void AutoOuterMinus_Clicked(object sender, EventArgs e) {
-			if (_data.AutoOuter < MAX) {
-				AutoInner.Text = (--_data.AutoOuter).ToString();
+			if (_data.AutoOuter > MIN) {
+				AutoOuter.Text = (--_data.AutoOuter).ToString();
 			}
 		}
 
 		//AUTO OUTER PLUS
 		private void AutoOuterPlus_Clicked(object sender, EventArgs e) {
 			if (_data.AutoOuter < MAX) {
-				AutoInner.Text = (++_data.AutoOuter).ToString();
+				AutoOuter.Text = (++_data.AutoOuter).ToString();
 			}
 		}
 
-		//AUTO LOWER 
+		//AUTO LOWER MINUS
 		private void AutoLowerMinus_Clicked(object sender, EventArgs e) {
-			if (_data.AutoLower < MAX) {
+			if (_data.AutoLower > MIN) {
 				AutoLower.Text = (--_data.AutoLower).ToString();
 			}
 		}
 
+		//AUTO LOWER PLUS
 		private void AutoLowerPlus_Clicked(object sender, EventArgs e) {
 			if (_data.AutoLower < MAX) {
 				AutoLower.Text = (++_data.AutoLower).ToString();
 			}
 		}
+
+		//AUTO MISSED MINUS
+		private void AutoMissedMinus_Clicked(object sender, EventArgs e) {
+			if (_data.AutoMissed > MIN) {
+				AutoMissed.Text = (--_data.AutoMissed).ToString();
+			}
+		}
+
+		//AUTO MISSED PLUS
+		private void AutoMissedPlus_Clicked(object sender, EventArgs e) {
+			if (_data.AutoMissed < MAX) {
+				AutoMissed.Text = (++_data.AutoMissed).ToString();
+			}
+		}
+
+		//AUTO DROPPED MINUS
+		private void AutoDroppedMinus_Clicked(object sender, EventArgs e) {
+			if (_data.AutoDropped > MIN) {
+				AutoDropped.Text = (--_data.AutoDropped).ToString();
+			}
+		}
+
+		//AUTO DROPPED PLUS
+		private void AutoDroppedPlus_Clicked(object sender, EventArgs e) {
+			if (_data.AutoDropped < MAX) {
+				AutoDropped.Text = (++_data.AutoDropped).ToString();
+			}
+		}
+
+		//AUTO COLLECTED MINUS
+		private void AutoCollectedMinus_Clicked(object sender, EventArgs e) {
+			if (_data.AutoCollected > MIN) {
+				AutoCollected.Text = (--_data.AutoCollected).ToString();
+			}
+		}
+
+		//AUTO COLLECTED PLUS
+		private void AutoCollectedPlus_Clicked(object sender, EventArgs e) {
+			if (_data.AutoCollected < MAX) {
+				AutoCollected.Text = (++_data.AutoCollected).ToString();
+			}
+		}
+		#endregion
+
+		#region Tele
+		//Tele INNER MINUS
+		private void TeleInnerMinus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleInner > MIN) {
+				TeleInner.Text = (--_data.TeleInner).ToString();
+			}
+		}
+
+		//Tele INNER PLUS
+		private void TeleInnerPlus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleInner < MAX) {
+				TeleInner.Text = (++_data.TeleInner).ToString();
+			}
+		}
+
+		//Tele OUTER MINUS
+		private void TeleOuterMinus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleOuter > MIN) {
+				TeleOuter.Text = (--_data.TeleOuter).ToString();
+			}
+		}
+
+		//Tele OUTER PLUS
+		private void TeleOuterPlus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleOuter < MAX) {
+				TeleOuter.Text = (++_data.TeleOuter).ToString();
+			}
+		}
+
+		//Tele LOWER MINUS
+		private void TeleLowerMinus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleLower > MIN) {
+				TeleLower.Text = (--_data.TeleLower).ToString();
+			}
+		}
+
+		//Tele LOWER PLUS
+		private void TeleLowerPlus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleLower < MAX) {
+				TeleLower.Text = (++_data.TeleLower).ToString();
+			}
+		}
+
+		//Tele MISSED MINUS
+		private void TeleMissedMinus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleMissed > MIN) {
+				TeleMissed.Text = (--_data.TeleMissed).ToString();
+			}
+		}
+
+		//Tele MISSED PLUS
+		private void TeleMissedPlus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleMissed < MAX) {
+				TeleMissed.Text = (++_data.TeleMissed).ToString();
+			}
+		}
+
+		//Tele DROPPED MINUS
+		private void TeleDroppedMinus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleDropped > MIN) {
+				TeleDropped.Text = (--_data.TeleDropped).ToString();
+			}
+		}
+
+		//Tele DROPPED PLUS
+		private void TeleDroppedPlus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleDropped < MAX) {
+				TeleDropped.Text = (++_data.TeleDropped).ToString();
+			}
+		}
+
+		//Tele COLLECTED MINUS
+		private void TeleCollectedMinus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleCollected > MIN) {
+				TeleCollected.Text = (--_data.TeleCollected).ToString();
+			}
+		}
+
+		//Tele COLLECTED PLUS
+		private void TeleCollectedPlus_Clicked(object sender, EventArgs e) {
+			if (_data.TeleCollected < MAX) {
+				TeleCollected.Text = (++_data.TeleCollected).ToString();
+			}
+		}
+
+
+
+
+		#endregion
+
+		#region End
+
+		private void FoulsMinus_Clicked(object sender, EventArgs e) {
+			if (_data.Fouls > MIN) {
+				Fouls.Text = (--_data.Fouls).ToString();
+			}
+		}
+
+		private void FoulsPlus_Clicked(object sender, EventArgs e) {
+			if (_data.Fouls < MAX) {
+				Fouls.Text = (++_data.Fouls).ToString();
+			}
+		}
+		#endregion
 	}
 }
